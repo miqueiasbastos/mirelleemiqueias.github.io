217a6f174606b43f4e81364b2a424cc123a889cf
miqueiasbastos <miqueiasbastos@hotmail.com>
2019-09-23 19:13:30 -0300

    a

---
+++ js/galeria.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	$('.fancybox-thumbs').fancybox({
+		prevEffect : 'none',
+		nextEffect : 'none',
+		padding: 0,
+		closeBtn  : true,
+		arrows    : true,
+		nextClick : true,
+		helpers : {
+			thumbs : {
+				width  : 50,
+				height : 50
+			}
+		}
+	});
+});
