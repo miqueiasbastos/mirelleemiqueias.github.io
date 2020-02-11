5a72cd3d202dd8db99bd1abf89d81ccb4080ad4c
miqueiasbastos <miqueiasbastos@hotmail.com>
2020-02-11 16:41:30 -0300

    Album

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function () {
+    var $galleryNoivado = $('#noivado a').simpleLightbox({
+        showCounter: false
+    });
+    var $galleryPreWedding = $('#pre-wedding a').simpleLightbox({
+        showCounter: false
+    });
+    var $galleryCasamento = $('#casamento a').simpleLightbox({
+        showCounter: false
+    });
+});
