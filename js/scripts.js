c2e0a464e40e0bf9e14bcae09bf400dff7a0e308
miqueiasbastos <miqueiasbastos@hotmail.com>
2020-02-11 18:12:31 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
 $(function () {
-    var $galleryNoivado = $('#noivado a').simpleLightbox({
-        showCounter: false,
-disableRightClick: false
+    var galleryNoivado = $('#noivado a').simpleLightbox({
+        showCounter: false
     });
-    var $galleryPreWedding = $('#pre-wedding a').simpleLightbox({
-        showCounter: false,
-disableRightClick: false
+    var galleryPreWedding = $('#pre-wedding a').simpleLightbox({
+        showCounter: false
     });
-    var $galleryCasamento = $('#casamento a').simpleLightbox({
-        showCounter: false,
-disableRightClick: false
+    var ggalleryCasamento = $('#casamento a').simpleLightbox({
+        showCounter: false
     });
 });
